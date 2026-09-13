ac6750cbfc81b35360f4db40aeac357e91229604
LibanAbdullahiM <libanr4243@gmail.com>
2026-09-13 13:49:13 +0300

    adding HomeController. closes #30

---
+++ src/main/java/com/liban/aisupporttickettriageapi/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.liban.aisupporttickettriageapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String home() {
+        return "Welcome to AIS Support Ticket Triage API!. Sorry We are under progress Yet." +
+                "We will be online soon. See soon!";
+    }
+}
